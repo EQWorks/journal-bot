f60e7796dadf8ba60dc6241bcb43a7e1509df534
woozyking <runzhou.li@gmail.com>
2020-01-06 12:48:32 -0500

    routine - fix syntax error on pastFilter, add missing yarn.lock deps

--- src/routine.js
+++ src/routine.js
@@ -3,8 +3,7 @@ const client = require('./client')
 
 const { ASANA_PROJECT = '1152701043959235' } = process.env
 
-const pastFilter = (task) => new Date(`${task.due_at || `${task.due_on}T23:59:59.999Z}`}`)
-  < new Date()
+const pastFilter = (t) => new Date(t.due_at || `${t.due_on}T23:59:59.999Z}`) < new Date()
 
 const markPastDue = (project_id = ASANA_PROJECT) => client.projects.tasks(project_id, {
   completed_since: 'now',
