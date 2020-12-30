0c445fa8267183c85b339bb79f3a5c419cb0f745
hyx131 <selin.hyx@gmail.com>
2020-12-30 14:14:40 -0500

    dev-journal - create new journals with auto-fill for last-work-day & tasks sections

--- src/dev-journal.js
+++ src/dev-journal.js
@@ -7,39 +7,10 @@ const subtractMe = isMonday ? 3 : 1
 const prevWorkDay = new Date(new Date().setDate(new Date().getDate() - subtractMe))
   .toISOString()
   .split('T')[0]
-const params = {
-  project: DEV_JOURNAL,
-  opt_fields: 'completed,projects.name,due_on,name,notes,subtasks,assignee.name,custom_fields',
-}
 
-/* NO TASK-NESTING FOR NOW */
-// get subTasks and children of subTasks
-// const getSubTasks = async (task, completed) => {
-//   const tasks = completed
-//      ? await getFilteredTasks(task, true)
-//      : await getFilteredTasks(task, false)
-//   if (tasks.length > 0) {
-//     const taskParam = tasks.map(async (t) => {
-//       const subChildTasks = completed
-//         ? await getFilteredTasks(t, true)
-//         : await getFilteredTasks(t, false)
-//       if (subChildTasks.length > 0) {
-//         const subChildParams = subChildTasks.map(async (child) => ({
-//           name: child.name,
-//           subTasks: await getSubTasks(child, completed),
-//         }))
-//         const childTasks = await Promise.all(subChildParams).then((r) => r)
-//         return { name: t.name, subTasks: childTasks }
-//       }
-//       return { name: t.name, subTasks: [] }
-//     })
-//     return Promise.all(taskParam).then((r) => r)
-//   }
-//   return []
-// }
-
-const _getJournals = () => async () => {
-  const _prevDayTasks = await client.tasks
+// get last-work-day journals
+const getLWDJournals = async () => {
+  const prevDayTasks = await client.tasks
     .getTasksForProject(
       DEV_JOURNAL,
       { opt_fields: 'due_on,custom_fields,name,assignee,projects,workspace' },
@@ -47,14 +18,14 @@ const _getJournals = () => async () => {
     .then((r) => r.data.filter(({ due_on }) => due_on === prevWorkDay))
     .catch((e) => console.error(`Failed to fetch prev-work-day tasks: ${e}`))
 
-  const _getSubTasks = (task) => client.tasks
+  const getSubTasks = (task) => client.tasks
     .getSubtasksForTask(task, { opt_fields: 'gid,name,completed,resource_type' })
     .then((r) => r.data)
     .catch((e) => console.error(`Failed to fetch subtasks: ${e}`))
 
-  const _taskArr = _prevDayTasks
+  return Promise.all(prevDayTasks
     .map(async ({ gid, name, assignee, projects, workspace, custom_fields }) => {
-      const subTasks = await _getSubTasks(gid)
+      const subTasks = await getSubTasks(gid)
       return {
         gid,
         name,
@@ -63,31 +34,50 @@ const _getJournals = () => async () => {
         workspace: workspace.gid,
         incompleteSubTasks: subTasks.filter((t) => !t.completed),
         completedSubTasks: subTasks.filter((t) => t.completed),
-        customField: custom_fields.find((c) => c.name === 'Last Workday'),
+        customField: custom_fields.find((c) => c.name === 'Last Workday').gid,
       }
-    })
-  return Promise.all(_taskArr)
+    }))
 }
 
-const _getFieldInfo = (params) => async (field) => {
-  const { data } = await client.tasks.findAll(params)
-  const prevDayTasks = data.filter((task) => (task.due_on && task.due_on === prevWorkDay))
-  let result
-  if (field === 'lwd') {
-    result = prevDayTasks.map((d) => {
-      const lastWorkday = d.custom_fields[0].text_value
-      if (lastWorkday) return { name: d.name, lastWorkday }
-      return undefined
-    })
+// format completed tasks into single string
+//    --> TODO: match format with updates
+const formatLWD = (tasks) => {
+  if (tasks.length) {
+    const notes = tasks.map((t) => t.name)
+    return `* ${notes.join('\n* ')}`
   }
-  if (field === 'des') {
-    result = prevDayTasks.map((d) => {
-      if (d.notes) return { name: d.name, description: d.notes }
-      return undefined
-    })
-  }
-  return result.filter((r) => r !== undefined)
+  return ''
 }
 
-module.exports.getJournals = _getJournals(params)
-module.exports.getFieldInfo = _getFieldInfo(params)
+// create new journals
+module.exports.createJournals = async () => {
+  try {
+    const prevJournals = await getLWDJournals()
+    prevJournals.map(async ({
+      name,
+      completedSubTasks,
+      incompleteSubTasks,
+      projects,
+      workspace,
+      customField,
+    }) => {
+      // create sub tasks for each journal (mind rate limiting due to tasks loop)
+      const createJournalTasks = (journal, tasks) => tasks
+        .forEach((t) => client.tasks.createSubtaskForTask(journal, { name: t.name }))
+
+      const createJournalParams = {
+        name: `TEST-${name}`,
+        completed: false,
+        due_on: `${new Date().toISOString().split('T')[0]}`,
+        projects: [projects],
+        workspace,
+        custom_fields: { [customField]: formatLWD(completedSubTasks) },
+      }
+
+      const newJournal = await client.tasks.createTask(createJournalParams)
+      createJournalTasks(newJournal?.gid, incompleteSubTasks)
+    })
+  } catch (e) {
+    console.error(`Failed to create new journals: ${e}`)
+  }
+}
