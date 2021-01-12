82e9fad7cfcf24bf059b66e1b0d61dd94e2113e8
hyx131 <selin.hyx@gmail.com>
2021-01-12 16:26:48 -0500

    journal/vacay - fetch tasks from vacay

--- src/dev-journal.js
+++ src/dev-journal.js
@@ -4,9 +4,40 @@ const client = require('./client')
 const isMonday = new Date().getDay() === 1
 const isWeekend = new Date().getDay() === 6 || new Date().getDay() === 0
 const subtractMe = isMonday ? 3 : 1
+const currentDay = `${new Date().toISOString().split('T')[0]}`
 const prevWorkDay = new Date(new Date().setDate(new Date().getDate() - subtractMe))
   .toISOString()
   .split('T')[0]
+
+const _checkDateRange = (start_on, due_on) => (date) => (
+  (date >= start_on && date <= due_on) || date === due_on
+)
+
+// for those who's on vacay current day --> skip
+// for those who's on vacay prev-day but not current day --> grab from day before vacay period
+const AVAIL_PROJECT = 1152701043959235
+const availCheck = async () => {
+  const [{ gid: VACAY_GID }] = await client.sections.getSectionsForProject(AVAIL_PROJECT)
+    .then(({ data }) => data.filter(({ name }) => name.toLowerCase().startsWith('vacation')))
+
+  const devAvails = await client.tasks.getTasksForSection(
+    VACAY_GID,
+    { limit: 50, opt_fields: 'start_on,due_on,name,assignee' },
+  ).then(({ data }) => {
+    const onVacayPrevDay = []
+    const onVacayCurrentDay = []
+    data.forEach((task) => {
+      const { start_on, due_on } = task
+      const isAway = _checkDateRange(start_on, due_on)
+      if (isAway(prevWorkDay)) onVacayPrevDay.push(task)
+      if (isAway(currentDay)) onVacayCurrentDay.push(task)
+    })
+    return { onVacayPrevDay, onVacayCurrentDay }
+  })
+
+  console.log('test', devAvails)
+  return devAvails
+}
 
 // get last-work-day journals
 const getLWDJournals = async (DEV_JOURNAL) => {
@@ -51,47 +82,48 @@ const formatLWD = (tasks) => {
 
 // create new journals
 module.exports.createJournals = async (DEV_JOURNAL) => {
-  if (isWeekend) {
-    return
-  }
-  try {
-    const prevJournals = await getLWDJournals(DEV_JOURNAL)
-    await Promise.all(prevJournals.map(async ({
-      name,
-      assignee,
-      completedSubTasks,
-      incompleteSubTasks,
-      projects,
-      workspace,
-      customField,
-    }) => {
-      const nameTransform = (name) => {
-        const m = name.match(/(?<person>.*)[(]\d+[)]$/)
-        if (m) {
-          const { groups: { person } } = m
-          return `${person.trim()} (${incompleteSubTasks.length})`
-        }
-        return name
-      }
-      const params = {
-        name: nameTransform(name),
-        assignee,
-        completed: false,
-        due_on: `${new Date().toISOString().split('T')[0]}`,
-        projects: [projects],
-        workspace,
-        custom_fields: { [customField]: formatLWD(completedSubTasks) },
-      }
-      const { gid } = await client.tasks.createTask(params)
-      if (!gid) {
-        return
-      }
+  await availCheck()
+  // if (isWeekend) {
+  //   return
+  // }
+  // try {
+  //   const prevJournals = await getLWDJournals(DEV_JOURNAL)
+  //   await Promise.all(prevJournals.map(async ({
+  //     name,
+  //     assignee,
+  //     completedSubTasks,
+  //     incompleteSubTasks,
+  //     projects,
+  //     workspace,
+  //     customField,
+  //   }) => {
+  //     const nameTransform = (name) => {
+  //       const m = name.match(/(?<person>.*)[(]\d+[)]$/)
+  //       if (m) {
+  //         const { groups: { person } } = m
+  //         return `${person.trim()} (${incompleteSubTasks.length})`
+  //       }
+  //       return name
+  //     }
+  //     const params = {
+  //       name: nameTransform(name),
+  //       assignee,
+  //       completed: false,
+  //       due_on: `${new Date().toISOString().split('T')[0]}`,
+  //       projects: [projects],
+  //       workspace,
+  //       custom_fields: { [customField]: formatLWD(completedSubTasks) },
+  //     }
+  //     const { gid } = await client.tasks.createTask(params)
+  //     if (!gid) {
+  //       return
+  //     }
 
-      await Promise.all(incompleteSubTasks.map((t) => (
-        client.tasks.createSubtaskForTask(gid, { name: t.name })
-      )))
-    }))
-  } catch (e) {
-    console.error(`Failed to create new journals: ${e}`)
-  }
+  //     await Promise.all(incompleteSubTasks.map((t) => (
+  //       client.tasks.createSubtaskForTask(gid, { name: t.name })
+  //     )))
+  //   }))
+  // } catch (e) {
+  //   console.error(`Failed to create new journals: ${e}`)
+  // }
 }
